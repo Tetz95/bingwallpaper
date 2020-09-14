b5945d47dd374f89fb7de1c065f490563a5cb9f1
Jason Herrlich <jherrlich@icloud.com>
2020-09-13 18:54:02 -0700

    Style guide readability improvements.

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Bing Wallpaper Downloader
-This is a utility that I found on the internet written by user [nonbeing](https://stackoverflow.com/users/376240/nonbeing) on stackoverflow.\
-It will download the current bing wallpaper in the directory the utility is ran from.
+This is a utility that I found on the internet written by user 
+[nonbeing](https://stackoverflow.com/users/376240/nonbeing) on 
+stackoverflow, and it will download the current bing wallpaper in the 
+directory the utility is ran from.
--- bingwallpaper.py
+++ bingwallpaper.py
@@ -5,21 +5,24 @@ BING_URI_BASE = "http://www.bing.com"
 BING_WALLPAPER_PATH = "/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US"
 
 # open the Bing HPImageArchive URI and ask for a JSON response
-resp = requests.get(BING_URI_BASE + BING_WALLPAPER_PATH)
+init_response = requests.get(BING_URI_BASE + BING_WALLPAPER_PATH)
 
-if resp.status_code == 200:
-    json_response = json.loads(resp.content)
+if init_response.status_code == 200:
+    json_response = json.loads(init_response.content)
     wallpaper_path = json_response['images'][0]['url']
     filename_int = wallpaper_path.split('&')[-3]
     filename = filename_int.split('=')[-1]
     wallpaper_uri = BING_URI_BASE + wallpaper_path
 
-    # open the actual wallpaper uri, and write the response as an image on the filesystem
+    # open the actual wallpaper uri, and write the response as an image
+    # on the filesystem
     response = requests.get(wallpaper_uri)
     if response.status_code == 200:
         with open(filename, 'wb') as f:
             f.write(response.content)
     else:
-        raise ValueError("[ERROR] non-200 response from Bing server for '{}'".format(wallpaper_uri))
-else: 
-    raise ValueError("[ERROR] non-200 response from Bing server for '{}'".format(BING_URI_BASE + BING_WALLPAPER_PATH))
+        raise ValueError("[ERROR] non-200 response from Bing server for '{}'".
+                         format(wallpaper_uri))
+else:
+    raise ValueError("[ERROR] non-200 response from Bing server for '{}'".
+                     format(BING_URI_BASE + BING_WALLPAPER_PATH))
